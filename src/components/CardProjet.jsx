c55e9a3a5b550fb554d9fd1e5077776452fead3b
Nicolas Serres <miracle@macbook-pro-de-nicolas.home>
2022-11-13 20:42:06 +0100

    change text in cardproject at bottom

--- src/components/CardProjet.jsx
+++ src/components/CardProjet.jsx
@@ -5,12 +5,12 @@ const CardProjet = ({ projet }) => {
   return (
     <a className="button_project" href={projet.pdf}>
       <h4 className="main_title_project">
-        <p className="appli">{projet.name}</p>
         <img
           className="python_svg"
           src={projet.img}
           alt={"logo" + projet.name}
         />
+        <p className="appli">{projet.name}</p>
       </h4>
     </a>
   );
